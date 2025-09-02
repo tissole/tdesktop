39caab5e2eb3ecdf45cfe43e4871f2908974a60f
tissole <78413495+tissole@users.noreply.github.com>
2025-09-02 16:28:03 +0300

    c3-new counter

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -3783,20 +3783,17 @@ void ApiWrap::sendFiles(
 				: nullptr),
 			uploadWithType,
 			to,
-			(GetEnhancedBool("caption_from_file_name")) 
-				? [&]() {
-					if (file.fileNameCaption.text.isEmpty() && !file.path.isEmpty()) {
-						QFileInfo fileInfo(file.path);
-						return TextWithTags{fileInfo.fileName(), {}};
-					}
-					return file.fileNameCaption;
-				}() 
-				: caption,
-			file.spoiler,
-			album));
-		if (!GetEnhancedBool("caption_from_file_name")) {
-			caption = TextWithTags();
-		}
+		    (GetEnhancedBool("caption_from_file_name")) 
+			    ? [&]() {
+				    if (file.fileNameCaption.text.isEmpty() && !file.path.isEmpty()) {
+					    QFileInfo fileInfo(file.path);
+					    return TextWithTags{fileInfo.fileName(), {}};
+				    }
+				    return file.fileNameCaption;
+			    }() 
+			    : caption,
+		    file.spoiler,
+		    album));
 	}
 	if (album) {
 		_sendingAlbums.emplace(album->groupId, album);
