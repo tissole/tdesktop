7e41cd0543312f1e270bb1ced307636f0dd94bb0
tissole <78413495+tissole@users.noreply.github.com>
2025-09-27 22:11:33 +0300

    Update prepare.py

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1624,8 +1624,8 @@ win:
         -nomake tests ^
         -platform win32-msvc
 
-    jom -j%NUMBER_OF_PROCESSORS%
-    jom -j%NUMBER_OF_PROCESSORS% install
+    jom -j %NUMBER_OF_PROCESSORS%
+    jom -j %NUMBER_OF_PROCESSORS% install
 mac:
     find ../../patches/qtbase_$QT -type f -print0 | sort -z | xargs -0 git apply
     cd ..
