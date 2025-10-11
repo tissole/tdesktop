487a27344509144bbce66f4c18fefa5aed0bb5ae
tissole <78413495+tissole@users.noreply.github.com>
2025-10-11 20:38:30 +0300

    custom column function for first item, fixex for grid tooltip and bubles

--- Telegram/SourceFiles/history/view/history_view_message.cpp
+++ Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -3499,7 +3499,7 @@ void Message::drawInfo(
 	case InfoDisplayType::Default:
 		infoRight -= st::msgPadding.right() - st::msgDateDelta.x();
 		infoBottom -= st::msgPadding.bottom() - st::msgDateDelta.y();
-		p.setPen(stm->historyFileNameFg);
+		p.setPen(stm->msgDateFg);
 	break;
 	case InfoDisplayType::Image:
 		infoRight -= st::msgDateImgDelta + st::msgDateImgPadding.x();
