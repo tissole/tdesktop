4e9920c4c51bf452130549aad37f1de208b197ec
tissole <78413495+tissole@users.noreply.github.com>
2025-12-16 19:30:00 +0200

    Update history_view_context_menu.cpp

--- Telegram/SourceFiles/history/view/history_view_context_menu.cpp
+++ Telegram/SourceFiles/history/view/history_view_context_menu.cpp
@@ -1691,14 +1691,15 @@ base::unique_qptr<Ui::PopupMenu> FillContextMenu(
 						if (safeItem) {
 							if (!list->showCopyRestriction(safeItem)) {
 								if (hasCaptionText) {
-									TextUtilities::SetClipboardText(TextForMimeData::Rich(std::move(request.selectedText)));
+									TextUtilities::SetClipboardText(request.selectedText);
 								} else if (!safeItem->originalText().empty()) {
-									TextUtilities::SetClipboardText(TextForMimeData::Rich(safeItem->originalText()));
+									auto originalText = safeItem->originalText();
+									TextUtilities::SetClipboardText(TextForMimeData::Rich(std::move(originalText)));
 								} else if (const auto media = view->media()) {
 									// Fallback to the media's general text if individual item has none
 									auto mediaText = media->selectedText(TextSelection(0, std::numeric_limits<uint16>::max()));
 									if (!mediaText.empty()) {
-										TextUtilities::SetClipboardText(TextForMimeData::Rich(std::move(mediaText)));
+										TextUtilities::SetClipboardText(mediaText);
 									} else {
 										TextUtilities::SetClipboardText(HistoryItemText(safeItem));
 									}
