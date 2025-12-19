8b75a721b1287e2f7878c2707db21367b6f8ad68
tissole <78413495+tissole@users.noreply.github.com>
2025-12-20 01:58:32 +0200

    fix 3.4

--- Telegram/SourceFiles/history/view/media/history_view_photo.h
+++ Telegram/SourceFiles/history/view/media/history_view_photo.h
@@ -30,7 +30,6 @@ namespace HistoryView {
 class Photo final : public File {
 public:
 	bool aboveTextByDefault() const override { return true; }
-	bool isBubbleTop() const override { return true; }
 public:
 	Photo(
 		not_null<Element*> parent,
