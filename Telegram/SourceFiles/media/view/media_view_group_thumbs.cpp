c71a75831db5f718558b7cbcc96eb6e46dac8f4e
tissole <78413495+tissole@users.noreply.github.com>
2025-12-25 03:15:59 +0200

    q3

--- Telegram/SourceFiles/media/view/media_view_group_thumbs.cpp
+++ Telegram/SourceFiles/media/view/media_view_group_thumbs.cpp
@@ -209,8 +209,6 @@ public:
 private:
 	QSize wantedPixSize() const;
 	void validateImage();
-	int currentLeft() const;
-	int currentWidth() const;
 	int finalLeft() const;
 	int finalWidth() const;
 	void animateTo(int left, int width);
