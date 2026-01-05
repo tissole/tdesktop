1c060e2112f61fc2d14baf9d73a9e9ee1c315847
tissole <78413495+tissole@users.noreply.github.com>
2026-01-05 22:46:58 +0200

    g9

--- Telegram/SourceFiles/history/view/media/history_view_document.cpp
+++ Telegram/SourceFiles/history/view/media/history_view_document.cpp
@@ -624,9 +624,10 @@ QSize Document::countOptimalSize() {
 		minHeight = visualBottomOfElement + 6;
 	}
 	
+	/* Redundant subtraction removed
 	if (!isBubbleTop()) {
 		minHeight -= st::msgFileTopMinus;
-	}
+	} */
 	
 	return { maxWidth, minHeight };
 }
@@ -689,15 +690,17 @@ QSize Document::countCurrentSize(int newWidth) {
 	if (!captioned && !hasTranscribe) {
 		auto result = File::countCurrentSize(newWidth);
 		result.setHeight(newHeight);
+		/* Redundant subtraction removed
 		if (!isBubbleTop()) {
 			result.setHeight(result.height() - st::msgFileTopMinus);
-		}
+		} */
 		return result;
 	}
 
+	/* Redundant subtraction removed
 	if (!isBubbleTop()) {
 		newHeight -= st::msgFileTopMinus;
-	}
+	} */
 
 	accumulate_min(newWidth, maxWidth());
 	return { newWidth, newHeight };
