c89481126dacfb3d069401b9bc2bbe74d805ace4
tissole <78413495+tissole@users.noreply.github.com>
2026-01-17 06:42:33 +0200

    g10 fix searched/quote flashing word and item in column

--- Telegram/build/qt_version.py
+++ Telegram/build/qt_version.py
@@ -4,6 +4,10 @@ def resolve(arch):
     if sys.platform == 'darwin':
         os.environ['QT'] = '6.2.12'
     elif sys.platform == 'win32':
-        print('Forcing Qt 6.10.1')
-        os.environ['QT'] = '6.10.1'
+        if arch == 'arm' or 'qt6' in sys.argv:
+            print('Choosing Qt 6.')
+            os.environ['QT'] = '6.9.1'
+        else:
+            print('Choosing Qt 5.')
+            os.environ['QT'] = '5.15.17'
     return True
