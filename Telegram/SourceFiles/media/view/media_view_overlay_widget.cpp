ba75933e764fa6d18122041c426ef1a3d90de23d
tissole <78413495+tissole@users.noreply.github.com>
2026-01-22 04:48:19 +0200

    g37 copy cap s photo full screen

--- Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
+++ Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -1518,9 +1518,6 @@ void OverlayWidget::refreshCaptionGeometry() {
 	if (_caption.isEmpty() && (!_stories || !_stories->repost())) {
 		_captionRect = QRect();
 		return;
-	} else if (_fullScreenVideo) {
-		_captionRect = QRect();
-		return;
 	}
 
 	if (_groupThumbs && _groupThumbs->hiding()) {
