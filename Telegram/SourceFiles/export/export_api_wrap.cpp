b2a7eab1bbf83388c33e10d71f16ae02a20ae5a4
tissole <78413495+tissole@users.noreply.github.com>
2026-01-29 21:44:36 +0200

    1 4 19 15tk 4f

--- Telegram/SourceFiles/export/export_api_wrap.cpp
+++ Telegram/SourceFiles/export/export_api_wrap.cpp
@@ -49,9 +49,9 @@ int GetChunkSizeForFile(int64 fileSize) {
 
 int GetConcurrentChunksForFile(int64 fileSize) {
 	if (fileSize > 300 * kMegabyte) {
-		return 6; // More concurrency for large files
+		return 4; // More concurrency for large files
 	}
-	return 6; // Less concurrency for smaller files
+	return 4; // Less concurrency for smaller files
 }
 
 
