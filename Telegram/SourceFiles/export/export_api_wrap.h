46f093b02207a7aa6894cdf66618e8f5162f35ac
tissole <78413495+tissole@users.noreply.github.com>
2026-03-23 04:37:10 +0200

    29-2

--- Telegram/SourceFiles/export/export_api_wrap.h
+++ Telegram/SourceFiles/export/export_api_wrap.h
@@ -409,7 +409,7 @@ private:
 		Fn<void(FnMut<void()>)> _runner;
 		std::shared_ptr<bool> _guard;
 		std::deque<FnMut<void()>> _taskQueue;
-		int _tokens = 10; // 
+		int _tokens = 2; // 
 		crl::time _lastRefresh = 0;
 		bool _retryScheduled = false;
 	};
