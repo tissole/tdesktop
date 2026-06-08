d085e8b57aee20add7c29b16827b1cca3bed083f
tissole <78413495+tissole@users.noreply.github.com>
2026-06-09 02:26:32 +0300

    Update data_histories.h

--- Telegram/SourceFiles/data/data_histories.h
+++ Telegram/SourceFiles/data/data_histories.h
@@ -114,9 +114,7 @@ public:
 		MTPmessages_SendMedia,
 		MTPmessages_SendInlineBotResult,
 		MTPmessages_SendMultiMedia,
-		MTPmessages_ForwardMessages,
-		MTPInvokeWithTakeout<MTPmessages_ForwardMessages>,
-		MTPInvokeWithTakeout<MTPmessages_SendMedia>>;
+		MTPmessages_ForwardMessages;
 	int sendPreparedMessage(
 		not_null<History*> history,
 		FullReplyTo replyTo,
